fa845c73780b3f06713c2526fa7f22f254c5b5ab
Ian Wright <55286208+Z80coder@users.noreply.github.com>
2022-11-02 15:09:21 +0000

    embryo not function and test

--- tests/test_hard_not.py
+++ tests/test_hard_not.py
@@ -1,5 +1,35 @@
 import pytest
+import jax
+import jax.numpy as jnp
 from neurallogic import hard_not
 
 def test_differentiable_hard_not():
-    assert hard_not.differentiable_hard_not(0.1, 0.6) == pytest.approx(0.42)
+    x = jnp.array([1.0])
+    w = jnp.array([0.0])
+    assert hard_not.differentiable_hard_not(x, w) == pytest.approx([0.0])
+    x = jnp.array([1.0])
+    w = jnp.array([1.0])
+    assert hard_not.differentiable_hard_not(x, w) == pytest.approx([1.0])
+    x = jnp.array([0.0])
+    w = jnp.array([0.0])
+    assert hard_not.differentiable_hard_not(x, w) == pytest.approx([1.0])
+    x = jnp.array([0.0])
+    w = jnp.array([1.0])
+    assert hard_not.differentiable_hard_not(x, w) == pytest.approx([0.0])
+    x = jnp.array([1.0, 0.0, 1.0, 0.0])
+    w = jnp.array([1.0, 1.0, 0.0, 0.0])
+    assert hard_not.differentiable_hard_not(x, w) == pytest.approx([1.0, 0.0, 0.0, 1.0])
+
+def test_threaded_differentiable_hard_not():
+    threaded_differentiable_hard_not = jax.vmap(hard_not.differentiable_hard_not, in_axes=1, out_axes=0)
+    x = jnp.array([1.0, 0.0, 1.0, 0.0])
+    w = jnp.array([1.0, 1.0, 0.0, 0.0])
+    xs = jnp.stack([x for _ in range(10)])
+    ws = jnp.stack([w for _ in range(10)])
+    result = threaded_differentiable_hard_not(xs, ws)
+    expected_result = jnp.stack([jnp.array([1.0, 0.0, 0.0, 1.0]) for _ in range(10)])
+    print("x = ", xs)
+    print("w = ", ws)
+    print("result = ", result)
+    print("expected result = ", expected_result)
+    assert jnp.array_equal(result, expected_result)
