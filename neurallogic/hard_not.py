2475f597ccb7c944d7c358b176f4dfbe67f6cc27
Ian Wright <55286208+Z80coder@users.noreply.github.com>
2022-11-03 16:39:16 +0000

    better names

--- neurallogic/hard_not.py
+++ neurallogic/hard_not.py
@@ -1,12 +1,16 @@
 import jax
 
-def hard_not(w, x):
+def soft_not(w, x):
     """
     w > 0.5 implies the not operation is active, else inactive
-    The corresponding hard logic is: (x AND w) || (! x AND ! w) or equivalently ! (x XOR w)
+    
+    Corresponding hard logic: 
+        (x AND w) || (! x AND ! w) 
+    or equivalently 
+        ! (x XOR w)
     """
     return 1.0 - w + x * (2.0 * w - 1.0)
 
-hard_not_neuron = jax.vmap(hard_not, 0, 0)
+soft_not_neuron = jax.vmap(soft_not, 0, 0)
 
-hard_not_layer = jax.vmap(hard_not_neuron, (0, None), 0)
+soft_not_layer = jax.vmap(soft_not_neuron, (0, None), 0)
--- tests/test_hard_not.py
+++ tests/test_hard_not.py
@@ -3,35 +3,36 @@ import jax
 import jax.numpy as jnp
 from neurallogic import hard_not
 
-def test_hard_not():
-    assert hard_not.hard_not(1.0, 0.0) == 0.0
-    assert hard_not.hard_not(1.0, 1.0) == 1.0
-    assert hard_not.hard_not(0.0, 0.0) == 1.0
-    assert hard_not.hard_not(0.0, 1.0) == 0.0
+def test_soft_not():
+    assert hard_not.soft_not(1.0, 0.0) == 0.0
+    assert hard_not.soft_not(1.0, 1.0) == 1.0
+    assert hard_not.soft_not(1.0, 1.0) == 1.0
+    assert hard_not.soft_not(0.0, 0.0) == 1.0
+    assert hard_not.soft_not(0.0, 1.0) == 0.0
 
-def test_hard_not_neuron():
+def test_soft_not_neuron():
     x = jnp.array([1.0, 1.0])
     w = jnp.array([1.0, 1.0])
-    assert jnp.array_equal(hard_not.hard_not(w, x), jnp.array([1.0, 1.0]))
+    assert jnp.array_equal(hard_not.soft_not(w, x), jnp.array([1.0, 1.0]))
     x = jnp.array([0.0, 0.0])
     w = jnp.array([0.0, 0.0])
-    assert jnp.array_equal(hard_not.hard_not(w, x), jnp.array([1.0, 1.0]))
+    assert jnp.array_equal(hard_not.soft_not(w, x), jnp.array([1.0, 1.0]))
     x = jnp.array([1.0, 0.0])
     w = jnp.array([0.0, 1.0])
-    assert jnp.array_equal(hard_not.hard_not(w, x), jnp.array([0.0, 0.0]))
+    assert jnp.array_equal(hard_not.soft_not(w, x), jnp.array([0.0, 0.0]))
     x = jnp.array([0.0, 1.0])
     w = jnp.array([1.0, 0.0])
-    assert jnp.array_equal(hard_not.hard_not(w, x), jnp.array([0.0, 0.0]))
+    assert jnp.array_equal(hard_not.soft_not(w, x), jnp.array([0.0, 0.0]))
     x = jnp.array([0.0, 1.0])
     w = jnp.array([0.0, 0.0])
-    assert jnp.array_equal(hard_not.hard_not(w, x), jnp.array([1.0, 0.0]))
+    assert jnp.array_equal(hard_not.soft_not(w, x), jnp.array([1.0, 0.0]))
     x = jnp.array([0.0, 1.0])
     w = jnp.array([1.0, 1.0])
-    assert jnp.array_equal(hard_not.hard_not(w, x), jnp.array([0.0, 1.0]))
+    assert jnp.array_equal(hard_not.soft_not(w, x), jnp.array([0.0, 1.0]))
 
-def test_hard_not_layer():
+def test_soft_not_layer():
     x = jnp.array([1.0, 0.0])
     w = jnp.array([[1.0, 1.0], [0.0, 1.0], [1.0, 0.0], [0.0, 0.0]])
     expected_value = jnp.array([[1.0, 0.0], [0.0, 0.0], [1.0, 1.0], [0.0, 1.0]])
-    result = hard_not.hard_not_layer(w, x)
+    result = hard_not.soft_not_layer(w, x)
     assert jnp.array_equal(result, expected_value)
