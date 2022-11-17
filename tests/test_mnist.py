be8da9cf4a249bbbab3baeb8b14d1076315576e9
Ian Wright <55286208+Z80coder@users.noreply.github.com>
2022-11-17 16:24:19 +0000

    add deps

---
+++ tests/test_mnist.py
@@ -0,0 +1,20 @@
+"""
+import tensorflow_datasets as tfds
+import jax
+import jax.numpy as jnp
+import tensorboard
+import ml_collections
+from absl import logging
+
+def get_datasets():
+  ds_builder = tfds.builder('mnist')
+  ds_builder.download_and_prepare()
+  train_ds = tfds.as_numpy(ds_builder.as_dataset(split='train', batch_size=-1))
+  test_ds = tfds.as_numpy(ds_builder.as_dataset(split='test', batch_size=-1))
+  train_ds['image'] = jnp.float32(train_ds['image']) / 255.
+  test_ds['image'] = jnp.float32(test_ds['image']) / 255.
+  return train_ds, test_ds
+
+def train_and_evaluate():
+    return 0
+"""
