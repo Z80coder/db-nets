f13608e5ddc32fb85f3f185d78a85a98e364ae33
Ian Wright <55286208+Z80coder@users.noreply.github.com>
2022-11-21 10:20:47 +0000

    sketch

--- tests/test_mnist.py
+++ tests/test_mnist.py
@@ -1,10 +1,91 @@
-"""
+import tensorflow as tf
 import tensorflow_datasets as tfds
+from clu import platform
+import tempfile
+import pathlib
 import jax
 import jax.numpy as jnp
+import numpy as np
+from flax import linen as nn
+from flax.training import train_state
 import tensorboard
 import ml_collections
 from absl import logging
+from neurallogic import (hard_and, hard_not, hard_or, harden, harden_layer,
+                         neural_logic_net, primitives)
+import optax
+
+
+"""MNIST test.
+Executes the training and evaluation loop for MNIST.
+The data is loaded using tensorflow_datasets.
+"""
+
+def net(type, x):
+    x = hard_or.or_layer(16, type, nn.initializers.uniform(1.0), jnp.float64)(x)
+    x = hard_and.and_layer(4, type, nn.initializers.uniform(1.0), jnp.float64)(x)
+    x = hard_not.not_layer(1, type, dtype=jnp.float64)(x)
+    x = primitives.nl_ravel(type)(x)
+    x = harden_layer.harden_layer(type)(x)
+    return x
+
+class CNN(nn.Module):
+  """A simple CNN model."""
+
+  @nn.compact
+  def __call__(self, x):
+    x = nn.Conv(features=32, kernel_size=(3, 3))(x)
+    x = nn.relu(x)
+    x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2))
+    x = nn.Conv(features=64, kernel_size=(3, 3))(x)
+    x = nn.relu(x)
+    x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2))
+    x = x.reshape((x.shape[0], -1))  # flatten
+    x = nn.Dense(features=256)(x)
+    x = nn.relu(x)
+    x = nn.Dense(features=10)(x)
+    return x
+
+@jax.jit
+def apply_model(state, images, labels):
+  """Computes gradients, loss and accuracy for a single batch."""
+  def loss_fn(params):
+    logits = state.apply_fn({'params': params}, images)
+    one_hot = jax.nn.one_hot(labels, 10)
+    loss = jnp.mean(optax.softmax_cross_entropy(logits=logits, labels=one_hot))
+    return loss, logits
+
+  grad_fn = jax.value_and_grad(loss_fn, has_aux=True)
+  (loss, logits), grads = grad_fn(state.params)
+  accuracy = jnp.mean(jnp.argmax(logits, -1) == labels)
+  return grads, loss, accuracy
+
+@jax.jit
+def update_model(state, grads):
+  return state.apply_gradients(grads=grads)
+
+def train_epoch(state, train_ds, batch_size, rng):
+  """Train for a single epoch."""
+  train_ds_size = len(train_ds['image'])
+  steps_per_epoch = train_ds_size // batch_size
+
+  perms = jax.random.permutation(rng, len(train_ds['image']))
+  perms = perms[:steps_per_epoch * batch_size]  # skip incomplete batch
+  perms = perms.reshape((steps_per_epoch, batch_size))
+
+  epoch_loss = []
+  epoch_accuracy = []
+
+  for perm in perms:
+    batch_images = train_ds['image'][perm, ...]
+    batch_labels = train_ds['label'][perm, ...]
+    grads, loss, accuracy = apply_model(state, batch_images, batch_labels)
+    state = update_model(state, grads)
+    epoch_loss.append(loss)
+    epoch_accuracy.append(accuracy)
+  train_loss = np.mean(epoch_loss)
+  train_accuracy = np.mean(epoch_accuracy)
+  return state, train_loss, train_accuracy
 
 def get_datasets():
   ds_builder = tfds.builder('mnist')
@@ -15,6 +96,83 @@ def get_datasets():
   test_ds['image'] = jnp.float32(test_ds['image']) / 255.
   return train_ds, test_ds
 
-def train_and_evaluate():
-    return 0
-"""
+def create_train_state(rng, config):
+  """Creates initial `TrainState`."""
+  # soft, hard, symbolic = neural_logic_net.net(net)
+  soft = CNN()
+  soft_weights = soft.init(rng, jnp.ones([1, 28, 28, 1]))
+  tx = optax.sgd(config.learning_rate, config.momentum)
+  return train_state.TrainState.create(apply_fn=soft.apply, params=soft_weights, tx=tx)
+
+def train_and_evaluate(config: ml_collections.ConfigDict,
+                       workdir: str) -> train_state.TrainState:
+  """Execute model training and evaluation loop.
+  Args:
+    config: Hyperparameter configuration for training and evaluation.
+    workdir: Directory where the tensorboard summaries are written to.
+  Returns:
+    The train state (which includes the `.params`).
+  """
+  train_ds, test_ds = get_datasets()
+  rng = jax.random.PRNGKey(0)
+
+  summary_writer = tensorboard.SummaryWriter(workdir)
+  summary_writer.hparams(dict(config))
+
+  rng, init_rng = jax.random.split(rng)
+  state = create_train_state(init_rng, config)
+
+  for epoch in range(1, config.num_epochs + 1):
+    rng, input_rng = jax.random.split(rng)
+    state, train_loss, train_accuracy = train_epoch(state, train_ds,
+                                                    config.batch_size,
+                                                    input_rng)
+    _, test_loss, test_accuracy = apply_model(state, test_ds['image'],
+                                              test_ds['label'])
+
+    logging.info(
+        'epoch:% 3d, train_loss: %.4f, train_accuracy: %.2f, test_loss: %.4f, test_accuracy: %.2f'
+        % (epoch, train_loss, train_accuracy * 100, test_loss,
+           test_accuracy * 100))
+
+    summary_writer.scalar('train_loss', train_loss, epoch)
+    summary_writer.scalar('train_accuracy', train_accuracy, epoch)
+    summary_writer.scalar('test_loss', test_loss, epoch)
+    summary_writer.scalar('test_accuracy', test_accuracy, epoch)
+
+  summary_writer.flush()
+  return state
+
+def get_config():
+  """Get the default hyperparameter configuration."""
+  config = ml_collections.ConfigDict()
+
+  config.learning_rate = 0.1
+  config.momentum = 0.9
+  config.batch_size = 128
+  config.num_epochs = 10
+  return config
+
+def test_mnist():
+  # Make sure tf does not allocate gpu memory.
+  tf.config.experimental.set_visible_devices([], "GPU")
+
+  # Define training configuration.
+  config = get_config()
+  config.num_epochs = 1
+  config.batch_size = 8
+
+  rng = jax.random.PRNGKey(0)
+  inputs = jnp.ones((1, 28, 28, 3), jnp.float32)
+  state = create_train_state(rng, config)
+
+  # Create a temporary directory where tensorboard metrics are written.
+  workdir = tempfile.mkdtemp()
+
+  # Go two directories up to the root of the flax directory.
+  root_dir = pathlib.Path(__file__).absolute()
+  data_dir = str(root_dir) + "/.tfds/metadata"  # pylint: disable=unused-variable
+
+  with tfds.testing.mock_data(num_examples=8, data_dir=data_dir):
+    train_and_evaluate(config=config, workdir=workdir)
+
