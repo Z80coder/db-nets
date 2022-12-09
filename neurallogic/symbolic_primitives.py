05400c490a0bb40d0efb5f45d1b2fd49c306c439
Ian Wright <55286208+Z80coder@users.noreply.github.com>
2022-12-09 17:16:44 +0000

    first successful symbolic evaluation

--- neurallogic/symbolic_primitives.py
+++ neurallogic/symbolic_primitives.py
@@ -3,11 +3,16 @@ from plum import dispatch
 
 @dispatch
 def binary_operator(operator: str, a: str, b: str) -> str:
-    return f"({a} {operator} {b})"
+    return f"{a} {operator} {b}"
 
 @dispatch
 def binary_operator(operator: str, a: numpy.ndarray, b: numpy.ndarray):
-    return numpy.vectorize(binary_operator)(operator, a, b)
+    #print("binary_operator", operator, a, b)
+    #print("element type", a.dtype, b.dtype)
+    r = numpy.vectorize(binary_operator, otypes=[object])(operator, a, b)
+    #print("r", r)
+    #print("element type", r.dtype)
+    return r
 
 def symbolic_eval(x):
     return numpy.vectorize(eval)(x)
--- tests/test_symbolic_primitives.py
+++ tests/test_symbolic_primitives.py
@@ -1,18 +1,17 @@
 import numpy
-from plum import dispatch, parametric, type_of
 
 from neurallogic import symbolic_primitives
 
 def test_binary_operator_str_str():
     output = symbolic_primitives.binary_operator("+", "1", "2")
-    expected = "(1 + 2)"
+    expected = "1 + 2"
     assert output == expected
 
 def test_binary_operator_vector_vector():
     x1 = numpy.array(["1", "2"])
     x2 = numpy.array(["3", "4"])
     output = symbolic_primitives.binary_operator("+", x1, x2)
-    expected = numpy.array(["(1 + 3)", "(2 + 4)"])
+    expected = numpy.array(["1 + 3", "2 + 4"])
     assert numpy.all(output == expected)
     eval_output = symbolic_primitives.symbolic_eval(output)
     numpy_output = numpy.add(symbolic_primitives.symbolic_eval(x1), symbolic_primitives.symbolic_eval(x2))
@@ -22,7 +21,7 @@ def test_binary_operator_matrix_vector():
     x1 = numpy.array([["1", "2"], ["3", "4"]])
     x2 = numpy.array(["5", "6"])
     output = symbolic_primitives.binary_operator("+", x1, x2)
-    expected = numpy.array([["(1 + 5)", "(2 + 6)"], ["(3 + 5)", "(4 + 6)"]])
+    expected = numpy.array([["1 + 5", "2 + 6"], ["3 + 5", "4 + 6"]])
     assert numpy.all(output == expected)
     eval_output = symbolic_primitives.symbolic_eval(output)
     numpy_output = numpy.add(symbolic_primitives.symbolic_eval(x1), symbolic_primitives.symbolic_eval(x2))
@@ -32,7 +31,7 @@ def test_binary_operator_vector_matrix():
     x1 = numpy.array(["1", "2"])
     x2 = numpy.array([["3", "4"], ["5", "6"]])
     output = symbolic_primitives.binary_operator("+", x1, x2)
-    expected = numpy.array([["(1 + 3)", "(2 + 4)"], ["(1 + 5)", "(2 + 6)"]])
+    expected = numpy.array([["1 + 3", "2 + 4"], ["1 + 5", "2 + 6"]])
     assert numpy.all(output == expected)
     eval_output = symbolic_primitives.symbolic_eval(output)
     numpy_output = numpy.add(symbolic_primitives.symbolic_eval(x1), symbolic_primitives.symbolic_eval(x2))
@@ -42,7 +41,7 @@ def test_binary_operator_matrix_matrix():
     x1 = numpy.array([["1", "2"], ["3", "4"]])
     x2 = numpy.array([["5", "6"], ["7", "8"]])
     output = symbolic_primitives.binary_operator("+", x1, x2)
-    expected = numpy.array([["(1 + 5)", "(2 + 6)"], ["(3 + 7)", "(4 + 8)"]])
+    expected = numpy.array([["1 + 5", "2 + 6"], ["3 + 7", "4 + 8"]])
     assert numpy.all(output == expected)
     eval_output = symbolic_primitives.symbolic_eval(output)
     numpy_output = numpy.add(symbolic_primitives.symbolic_eval(x1), symbolic_primitives.symbolic_eval(x2))
@@ -54,7 +53,7 @@ def test_binary_operator_matrix_matrix_2():
     # x2 is a (10, 4) matrix
     x2 = numpy.array([["5", "6", "7", "8"] for _ in range(10)])
     output = symbolic_primitives.binary_operator("+", x1, x2)
-    expected = numpy.array([["(1 + 5)", "(2 + 6)", "(3 + 7)", "(4 + 8)"] for _ in range(10)])
+    expected = numpy.array([["1 + 5", "2 + 6", "3 + 7", "4 + 8"] for _ in range(10)])
     assert numpy.all(output == expected)
     eval_output = symbolic_primitives.symbolic_eval(output)
     numpy_output = numpy.add(symbolic_primitives.symbolic_eval(x1), symbolic_primitives.symbolic_eval(x2))
