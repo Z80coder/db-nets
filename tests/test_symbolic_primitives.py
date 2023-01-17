670f052a2800bb91e965e615bc4ac000a05cdbf8
Ian Wright <55286208+Z80coder@users.noreply.github.com>
2023-01-17 13:45:31 +0000

    get test_symbolic_primitives working again

--- tests/test_symbolic_primitives.py
+++ tests/test_symbolic_primitives.py
@@ -1,14 +1,14 @@
 import numpy
 import jax
-from neurallogic import symbolic_primitives
+from neurallogic import symbolic_primitives, sym_gen
 
 
 def test_unary_operator_str():
     output = symbolic_primitives.unary_operator("not", "True")
     expected = "not(True)"
     assert output == expected
-    eval_output = symbolic_primitives.eval_symbolic_expression(output)
-    eval_expected = symbolic_primitives.eval_symbolic_expression(expected)
+    eval_output = sym_gen.eval_symbolic_expression(output)
+    eval_expected = sym_gen.eval_symbolic_expression(expected)
     assert eval_output == eval_expected
 
 
@@ -17,8 +17,8 @@ def test_unary_operator_vector():
     output = symbolic_primitives.unary_operator("not", x)
     expected = numpy.array(["not(True)", "not(False)"])
     assert numpy.array_equal(output, expected)
-    eval_output = symbolic_primitives.eval_symbolic_expression(output)
-    eval_expected = symbolic_primitives.eval_symbolic_expression(expected)
+    eval_output = sym_gen.eval_symbolic_expression(output)
+    eval_expected = sym_gen.eval_symbolic_expression(expected)
     assert numpy.array_equal(eval_output, eval_expected)
 
 
@@ -28,8 +28,8 @@ def test_unary_operator_matrix():
     expected = numpy.array(
         [["not(True)", "not(False)"], ["not(False)", "not(True)"]])
     assert numpy.array_equal(output, expected)
-    eval_output = symbolic_primitives.eval_symbolic_expression(output)
-    eval_expected = symbolic_primitives.eval_symbolic_expression(expected)
+    eval_output = sym_gen.eval_symbolic_expression(output)
+    eval_expected = sym_gen.eval_symbolic_expression(expected)
     assert numpy.array_equal(eval_output, eval_expected)
 
 
@@ -37,9 +37,9 @@ def test_binary_operator_str_str():
     output = symbolic_primitives.binary_infix_operator("+", "1", "2")
     expected = "1 + 2"
     assert output == expected
-    eval_output = symbolic_primitives.eval_symbolic_expression(output)
-    numpy_output = numpy.add(symbolic_primitives.eval_symbolic_expression(
-        "1"), symbolic_primitives.eval_symbolic_expression("2"))
+    eval_output = sym_gen.eval_symbolic_expression(output)
+    numpy_output = numpy.add(sym_gen.eval_symbolic_expression(
+        "1"), sym_gen.eval_symbolic_expression("2"))
     assert numpy.array_equal(eval_output, numpy_output)
 
 
@@ -49,9 +49,9 @@ def test_binary_operator_vector_vector():
     output = symbolic_primitives.binary_infix_operator("+", x1, x2)
     expected = numpy.array(["1 + 3", "2 + 4"])
     assert numpy.array_equal(output, expected)
-    eval_output = symbolic_primitives.eval_symbolic_expression(output)
-    numpy_output = numpy.add(symbolic_primitives.eval_symbolic_expression(
-        x1), symbolic_primitives.eval_symbolic_expression(x2))
+    eval_output = sym_gen.eval_symbolic_expression(output)
+    numpy_output = numpy.add(sym_gen.eval_symbolic_expression(
+        x1), sym_gen.eval_symbolic_expression(x2))
     assert numpy.array_equal(eval_output, numpy_output)
 
 
@@ -61,9 +61,9 @@ def test_binary_operator_matrix_vector():
     output = symbolic_primitives.binary_infix_operator("+", x1, x2)
     expected = numpy.array([["1 + 5", "2 + 6"], ["3 + 5", "4 + 6"]])
     assert numpy.array_equal(output, expected)
-    eval_output = symbolic_primitives.eval_symbolic_expression(output)
-    numpy_output = numpy.add(symbolic_primitives.eval_symbolic_expression(
-        x1), symbolic_primitives.eval_symbolic_expression(x2))
+    eval_output = sym_gen.eval_symbolic_expression(output)
+    numpy_output = numpy.add(sym_gen.eval_symbolic_expression(
+        x1), sym_gen.eval_symbolic_expression(x2))
     assert numpy.array_equal(eval_output, numpy_output)
 
 
@@ -73,9 +73,9 @@ def test_binary_operator_vector_matrix():
     output = symbolic_primitives.binary_infix_operator("+", x1, x2)
     expected = numpy.array([["1 + 3", "2 + 4"], ["1 + 5", "2 + 6"]])
     assert numpy.array_equal(output, expected)
-    eval_output = symbolic_primitives.eval_symbolic_expression(output)
-    numpy_output = numpy.add(symbolic_primitives.eval_symbolic_expression(
-        x1), symbolic_primitives.eval_symbolic_expression(x2))
+    eval_output = sym_gen.eval_symbolic_expression(output)
+    numpy_output = numpy.add(sym_gen.eval_symbolic_expression(
+        x1), sym_gen.eval_symbolic_expression(x2))
     assert numpy.array_equal(eval_output, numpy_output)
 
 
@@ -85,9 +85,9 @@ def test_binary_operator_matrix_matrix():
     output = symbolic_primitives.binary_infix_operator("+", x1, x2)
     expected = numpy.array([["1 + 5", "2 + 6"], ["3 + 7", "4 + 8"]])
     assert numpy.array_equal(output, expected)
-    eval_output = symbolic_primitives.eval_symbolic_expression(output)
-    numpy_output = numpy.add(symbolic_primitives.eval_symbolic_expression(
-        x1), symbolic_primitives.eval_symbolic_expression(x2))
+    eval_output = sym_gen.eval_symbolic_expression(output)
+    numpy_output = numpy.add(sym_gen.eval_symbolic_expression(
+        x1), sym_gen.eval_symbolic_expression(x2))
     assert numpy.array_equal(eval_output, numpy_output)
 
 
@@ -100,9 +100,9 @@ def test_binary_operator_matrix_matrix_2():
     expected = numpy.array(
         [["1 + 5", "2 + 6", "3 + 7", "4 + 8"] for _ in range(10)])
     assert numpy.array_equal(output, expected)
-    eval_output = symbolic_primitives.eval_symbolic_expression(output)
-    numpy_output = numpy.add(symbolic_primitives.eval_symbolic_expression(
-        x1), symbolic_primitives.eval_symbolic_expression(x2))
+    eval_output = sym_gen.eval_symbolic_expression(output)
+    numpy_output = numpy.add(sym_gen.eval_symbolic_expression(
+        x1), sym_gen.eval_symbolic_expression(x2))
     assert numpy.array_equal(eval_output, numpy_output)
 
 
@@ -139,76 +139,36 @@ def test_to_boolean_value_string():
     assert output == expected
 
 
-def test_to_boolean_symbolic_values():
-    output = symbolic_primitives.make_symbolic([1, 1])
-    expected = ["True", "True"]
-    assert numpy.array_equal(output, expected)
-    output = symbolic_primitives.make_symbolic([0, 0])
-    expected = ["False", "False"]
-    assert numpy.array_equal(output, expected)
-    output = symbolic_primitives.make_symbolic([True, False])
-    expected = ["True", "False"]
-    assert numpy.array_equal(output, expected)
-    output = symbolic_primitives.make_symbolic([False, True])
-    expected = ["False", "True"]
-    assert numpy.array_equal(output, expected)
-    output = symbolic_primitives.make_symbolic([1.0, 1.0])
-    expected = ["True", "True"]
-    assert numpy.array_equal(output, expected)
-    output = symbolic_primitives.make_symbolic([0.0, 0.0])
-    expected = ["False", "False"]
-    assert numpy.array_equal(output, expected)
-    output = symbolic_primitives.make_symbolic([[1, 1], [1, 1]])
-    expected = [["True", "True"], ["True", "True"]]
-    assert numpy.array_equal(output, expected)
-    output = symbolic_primitives.make_symbolic([[0, 0], [0, 0]])
-    expected = [["False", "False"], ["False", "False"]]
-    assert numpy.array_equal(output, expected)
-    output = symbolic_primitives.make_symbolic(
-        [[True, False], [False, True]])
-    expected = [["True", "False"], ["False", "True"]]
-    assert numpy.array_equal(output, expected)
-    output = symbolic_primitives.make_symbolic(
-        [[[1, 0, 1], [1, 0, 1]], [[1, 0, 0], [1, 0, 0]]])
-    expected = [[["True", "False", "True"], ["True", "False", "True"]], [
-        ["True", "False", "False"], ["True", "False", "False"]]]
-    assert numpy.array_equal(output, expected)
-    output = symbolic_primitives.make_symbolic(
-        [[[1, "f", 1], [1, "g", 1]], [[1, "h", 0], [1, "f", 0]]])
-    expected = [[["True", "f", "True"], ["True", "g", "True"]], [
-        ["True", "h", "False"], ["True", "f", "False"]]]
-    assert numpy.array_equal(output, expected)
-
 
 def test_symbolic_eval():
-    output = symbolic_primitives.eval_symbolic_expression("1 + 2")
+    output = sym_gen.eval_symbolic_expression("1 + 2")
     expected = 3
     assert output == expected
-    output = symbolic_primitives.eval_symbolic_expression("[1, 2, 3]")
+    output = sym_gen.eval_symbolic_expression("[1, 2, 3]")
     expected = [1, 2, 3]
     assert numpy.array_equal(output, expected)
-    output = symbolic_primitives.eval_symbolic_expression("[1, 2, 3] + [4, 5, 6]")
+    output = sym_gen.eval_symbolic_expression("[1, 2, 3] + [4, 5, 6]")
     expected = [1, 2, 3, 4, 5, 6]
     assert numpy.array_equal(output, expected)
-    output = symbolic_primitives.eval_symbolic_expression(['1', '2', '3'])
+    output = sym_gen.eval_symbolic_expression(['1', '2', '3'])
     expected = [1, 2, 3]
     assert numpy.array_equal(output, expected)
-    output = symbolic_primitives.eval_symbolic_expression(
+    output = sym_gen.eval_symbolic_expression(
         ['1', '2', '3'] + ['4', '5', '6'])
     expected = [1, 2, 3, 4, 5, 6]
     assert numpy.array_equal(output, expected)
-    output = symbolic_primitives.eval_symbolic_expression(['not(False)', 'not(True)'])
+    output = sym_gen.eval_symbolic_expression(['not(False)', 'not(True)'])
     expected = [True, False]
     assert numpy.array_equal(output, expected)
-    output = symbolic_primitives.eval_symbolic_expression(
+    output = sym_gen.eval_symbolic_expression(
         [['not(False)', 'not(True)'] + ['not(False)', 'not(True)']])
     expected = [[True, False, True, False]]
     assert numpy.array_equal(output, expected)
-    output = symbolic_primitives.eval_symbolic_expression(numpy.array(
+    output = sym_gen.eval_symbolic_expression(numpy.array(
         [['not(False)', 'not(True)'] + ['not(False)', 'not(True)']]))
     expected = [[True, False, True, False]]
     assert numpy.array_equal(output, expected)
-    output = symbolic_primitives.eval_symbolic_expression(numpy.array(
+    output = sym_gen.eval_symbolic_expression(numpy.array(
         [['not(False)', False], ['not(False)', 'not(True)']]))
     expected = [[True, False], [True, False]]
     assert numpy.array_equal(output, expected)
@@ -219,7 +179,7 @@ def test_symbolic_not():
     output = symbolic_primitives.symbolic_not(x1)
     expected = numpy.array([False, True])
     assert numpy.array_equal(output, expected)
-    x1 = symbolic_primitives.make_symbolic(x1)
+    x1 = sym_gen.make_symbolic(x1)
     output = symbolic_primitives.symbolic_not(x1)
     expected = numpy.array(["not(True)", "not(False)"])
     assert numpy.array_equal(output, expected)
@@ -231,8 +191,8 @@ def test_symbolic_and():
     output = symbolic_primitives.symbolic_and(x1, x2)
     expected = numpy.array([True, False])
     assert numpy.array_equal(output, expected)
-    x1 = symbolic_primitives.make_symbolic(x1)
-    x2 = symbolic_primitives.make_symbolic(x2)
+    x1 = sym_gen.make_symbolic(x1)
+    x2 = sym_gen.make_symbolic(x2)
     output = symbolic_primitives.symbolic_and(x1, x2)
     expected = numpy.array(["True and True", "False and True"])
     assert numpy.array_equal(output, expected)
@@ -244,10 +204,10 @@ def test_symbolic_xor():
     output = symbolic_primitives.symbolic_xor(x1, x2)
     expected = numpy.array([False, True])
     assert numpy.array_equal(output, expected)
-    x1 = symbolic_primitives.make_symbolic(x1)
-    x2 = symbolic_primitives.make_symbolic(x2)
+    x1 = sym_gen.make_symbolic(x1)
+    x2 = sym_gen.make_symbolic(x2)
     output = symbolic_primitives.symbolic_xor(x1, x2)
-    expected = numpy.array(["(True) ^ (True)", "(False) ^ (True)"])
+    expected = numpy.array(["True ^ True", "False ^ True"])
     assert numpy.array_equal(output, expected)
 
 
@@ -282,40 +242,40 @@ def symbolic_reduce_or_impl(input, expected, symbolic_expected, axes):
     expected = numpy.array(expected)
     assert numpy.array_equal(output, expected)
     # Test symbolic implementation
-    input = symbolic_primitives.make_symbolic(input)
+    input = sym_gen.make_symbolic(input)
     output = symbolic_primitives.symbolic_reduce_or(input, axes=axes)
     symbolic_expected = numpy.array(symbolic_expected)
     assert numpy.array_equal(output, symbolic_expected)
     # Compare the reference and symbolic evaluation
-    symbolic_expected = symbolic_primitives.eval_symbolic_expression(symbolic_expected)
+    symbolic_expected = sym_gen.eval_symbolic_expression(symbolic_expected)
     assert numpy.array_equal(expected, symbolic_expected)
 
 
 def test_symbolic_reduce_or():
     # Test 1: 2D matrix with different axes inputs
     symbolic_reduce_or_impl(input=[[True, False], [True, False]], expected=[
-                            True, True], symbolic_expected=['False or True or False', 'False or True or False'], axes=(1,))
+                            True, True], symbolic_expected=['((False or True) or False)', '((False or True) or False)'], axes=(1,))
     symbolic_reduce_or_impl(input=[[True, False], [True, False]], expected=[
-                            True, False], symbolic_expected=['False or True or True', 'False or False or False'], axes=(0,))
+                            True, False], symbolic_expected=['((False or True) or True)', '((False or False) or False)'], axes=(0,))
     symbolic_reduce_or_impl(input=[[True, False], [True, False]], expected=True,
-                            symbolic_expected='False or True or False or True or False', axes=(0, 1))
+                            symbolic_expected='((((False or True) or False) or True) or False)', axes=(0, 1))
     # Test 2: 3D matrix with different axes inputs
-    symbolic_reduce_or_impl(input=[[[True, False], [True, False]], [[True, False], [True, False]]], expected=[True, True], symbolic_expected=[
-                            'False or True or False or True or False', 'False or True or False or True or False'], axes=(1, 2))
-    symbolic_reduce_or_impl(input=[[[True, False], [True, False]], [[True, False], [True, False]]], expected=[True, True], symbolic_expected=[
-                            'False or True or False or True or False', 'False or True or False or True or False'], axes=(0, 2))
-    symbolic_reduce_or_impl(input=[[[True, False], [True, False]], [[True, False], [True, False]]], expected=[True, False], symbolic_expected=[
-                            'False or True or True or True or True', 'False or False or False or False or False'], axes=(0, 1))
+    symbolic_reduce_or_impl(input=[[[True, False], [True, False]], [[True, False], [True, False]]], expected=[True, True], symbolic_expected=
+                            ['((((False or True) or False) or True) or False)', '((((False or True) or False) or True) or False)'], axes=(1, 2))
+    symbolic_reduce_or_impl(input=[[[True, False], [True, False]], [[True, False], [True, False]]], expected=[True, True], symbolic_expected=
+                            ['((((False or True) or False) or True) or False)', '((((False or True) or False) or True) or False)'], axes=(0, 2))
+    symbolic_reduce_or_impl(input=[[[True, False], [True, False]], [[True, False], [True, False]]], expected=[True, False], symbolic_expected=
+                            ['((((False or True) or True) or True) or True)', '((((False or False) or False) or False) or False)'], axes=(0, 1))
     symbolic_reduce_or_impl(input=[[[True, False], [True, False]], [[True, False], [True, False]]], expected=True,
-                            symbolic_expected='False or True or False or True or False or True or False or True or False', axes=(0, 1, 2))
+                            symbolic_expected='((((((((False or True) or False) or True) or False) or True) or False) or True) or False)', axes=(0, 1, 2))
     # Test 3: 4D matrix with different axes inputs
-    symbolic_reduce_or_impl(input=[[[[True, False], [True, False]], [[True, False], [True, False]]], [[[True, False], [True, False]], [[True, False], [True, False]]]], expected=[True, True], symbolic_expected=[
-                            'False or True or False or True or False or True or False or True or False', 'False or True or False or True or False or True or False or True or False'], axes=(1, 2, 3))
-    symbolic_reduce_or_impl(input=[[[[True, False], [True, False]], [[True, False], [True, False]]], [[[True, False], [True, False]], [[True, False], [True, False]]]], expected=[True, True], symbolic_expected=[
-                            'False or True or False or True or False or True or False or True or False', 'False or True or False or True or False or True or False or True or False'], axes=(0, 2, 3))
-    symbolic_reduce_or_impl(input=[[[[True, False], [True, False]], [[True, False], [True, False]]], [[[True, False], [True, False]], [[True, False], [True, False]]]], expected=[True, True], symbolic_expected=[
-                            'False or True or False or True or False or True or False or True or False', 'False or True or False or True or False or True or False or True or False'], axes=(0, 1, 3))
-    symbolic_reduce_or_impl(input=[[[[True, False], [True, False]], [[True, False], [True, False]]], [[[True, False], [True, False]], [[True, False], [True, False]]]], expected=[True, False], symbolic_expected=[
-                            'False or True or True or True or True or True or True or True or True', 'False or False or False or False or False or False or False or False or False'], axes=(0, 1, 2))
+    symbolic_reduce_or_impl(input=[[[[True, False], [True, False]], [[True, False], [True, False]]], [[[True, False], [True, False]], [[True, False], [True, False]]]], expected=[True, True], symbolic_expected=
+                            ['((((((((False or True) or False) or True) or False) or True) or False) or True) or False)', '((((((((False or True) or False) or True) or False) or True) or False) or True) or False)'], axes=(1, 2, 3))
+    symbolic_reduce_or_impl(input=[[[[True, False], [True, False]], [[True, False], [True, False]]], [[[True, False], [True, False]], [[True, False], [True, False]]]], expected=[True, True], symbolic_expected=
+                            ['((((((((False or True) or False) or True) or False) or True) or False) or True) or False)', '((((((((False or True) or False) or True) or False) or True) or False) or True) or False)'], axes=(0, 2, 3))
+    symbolic_reduce_or_impl(input=[[[[True, False], [True, False]], [[True, False], [True, False]]], [[[True, False], [True, False]], [[True, False], [True, False]]]], expected=[True, True], symbolic_expected=
+                            ['((((((((False or True) or False) or True) or False) or True) or False) or True) or False)', '((((((((False or True) or False) or True) or False) or True) or False) or True) or False)'], axes=(0, 1, 3))
+    symbolic_reduce_or_impl(input=[[[[True, False], [True, False]], [[True, False], [True, False]]], [[[True, False], [True, False]], [[True, False], [True, False]]]], expected=[True, False], symbolic_expected=
+                            ['((((((((False or True) or True) or True) or True) or True) or True) or True) or True)', '((((((((False or False) or False) or False) or False) or False) or False) or False) or False)'], axes=(0, 1, 2))
     symbolic_reduce_or_impl(input=[[[[True, False], [True, False]], [[True, False], [True, False]]], [[[True, False], [True, False]], [[True, False], [True, False]]]], expected=True,
-                            symbolic_expected='False or True or False or True or False or True or False or True or False or True or False or True or False or True or False or True or False', axes=(0, 1, 2, 3))
+                            symbolic_expected='((((((((((((((((False or True) or False) or True) or False) or True) or False) or True) or False) or True) or False) or True) or False) or True) or False) or True) or False)', axes=(0, 1, 2, 3))
